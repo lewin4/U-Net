63769c6f252993fe73d7b9e952c7a2cb5ef5660b
LiuYan <mfxjo5@outlook.com>
2021-08-13 14:40:28 +0800

    使用单精度，模型验证计算IOU，每一轮训练完成验证IOU分数，保存IOU最高的模型。

--- train.py
+++ train.py
@@ -2,18 +2,20 @@ import torch
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from tqdm import tqdm
-import torch.nn as nn
 import torch.optim as optim
 from model import UNet
 from dataloader import get_loaders
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
 from utils import (load_checkpoint, save_checkpoint, check_accuracy, save_predictions_as_imgs, DiceLoss)
 
 # Hyperparameters etc.
 LEARNING_RATE = 5e-5
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-BATCH_SIZE = 4
-NUM_EPOCHS = 30
+BATCH_SIZE = 6
+NUM_EPOCHS = 20
 NUM_WORKER = 8
 IMAGE_WIDTH = 1024
 IMAGE_HEIGHT = 768
@@ -26,33 +28,38 @@ MASK_DIR = r"E:\LY\data\sewage\small_dataset\small_label"
 def train_fn(loader, model, optimizer, loss_fn, epoch, scaler):
     loop = tqdm(loader)
     loop.set_description(f"Epoch {epoch} train")
+    losses = []
     for data, targets in loop:
         data = data.to(DEVICE)
         targets = targets.float().unsqueeze(1).to(DEVICE)
 
-        # # forward
-        # with torch.cuda.amp.autocast():
-        #     predictions = model(data)
-        #     loss = loss_fn(predictions, targets)
-        #
-        # # backward
-        # optimizer.zero_grad()
-        # scaler.scale(loss).backward()
-        # scaler.step(optimizer)
-        # scaler.update()
-
         # forward
-        predictions = model(data)
-        loss = loss_fn(predictions, targets)
+        with torch.cuda.amp.autocast():
+            predictions = model(data)
+            loss = loss_fn(predictions, targets)
+            losses.append(loss)
 
         # backward
         optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        # # forward
+        # predictions = model(data)
+        # loss = loss_fn(predictions, targets)
+        # losses.append(loss)
+        #
+        # # backward
+        # optimizer.zero_grad()
+        # loss.backward()
+        # optimizer.step()
 
         # update tqdm loop
         loop.set_postfix(loss=loss.item())
 
+    #compute loss mean
+    logging.info(f"Epoch {epoch}'s mean train loss:{sum(losses)/len(losses)}")
 
 def main():
     train_transform = A.Compose(
@@ -101,13 +108,18 @@ def main():
         load_checkpoint(torch.load("output/checkpoints/checkpoint.pth"), model)
         check_accuracy(val_loader, model, device=DEVICE)
 
+    best_score = 0
     scalar = torch.cuda.amp.GradScaler()
     for epoch in range(NUM_EPOCHS):
         train_fn(train_loader, model, optimizer, loss_fn, epoch, scalar)
 
-        # check accuracy
-        check_accuracy(val_loader, model, device=DEVICE, epoch=epoch)
+        # every epoch check accuracy and save the best model
+        iou_score = check_accuracy(val_loader, model, device=DEVICE, epoch=epoch)
+        if iou_score >= best_score:
+            save_checkpoint(model, optimizer, filename=f"output/checkpoints/best_checkpoint.pth")
+            best_score = iou_score
 
+    logging.info(f"Train done.\nThe best iou score: {best_score}.")
     # save model
     save_checkpoint(model, optimizer, filename="output/checkpoints/checkpoint.pth")
 
